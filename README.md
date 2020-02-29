6e753c5a7382ab3a436e34c5bb08104e1e56dccd
Kolton Yager <kjyager@calpoly.edu>
2020-02-29 01:15:33 -0800

    Gave up on external optional substitute

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Personal collection of standalone vulkan utilities
 
 ## Dependencies
 - Vulkan
-- C++ 17 or partial implementation of `<optional>` accessible through `std::`
 - Vulkan [Memory Allocator library](https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator)
--- optional.h
+++ optional.h
@@ -1,12 +1,120 @@
-#ifndef VKUTILS_OPTIONAL_INCLUDER_H_
-#define VKUTILS_OPTIONAL_INCLUDER_H_
+#pragma once
+#ifndef KJY_VKUTILS_OPTIONAL_SUBSTITUTE_H_
+#define KJY_VKUTILS_OPTIONAL_SUBSTITUTE_H_
 
-#if __cplusplus >= 201700L
-    #include <optional>
-#elif defined VKUTILS_OPTIONAL_SUBSTITUTE
-    #include VKUTILS_OPTIONAL_SUBSTITUTE
+#if __cplusplus >= 201703L
+#include <optional>
 #else
-    #error "vkutils could not find a valid std::optional implemntation. Please use c++ 17 or provide your own substitute and set it's include path as VKUTILS_OPTIONAL_SUBSTITUTE"
-#endif
+
+#include <exception>
+#include <cstdint>
+
+namespace std{
+
+
+struct nullopt_t
+{
+    explicit constexpr nullopt_t(int) {}
+};
+
+constexpr nullopt_t nullopt{42};
+
+class bad_optional_access : std::exception
+{
+ public:
+    virtual const char* what() const noexcept{
+        return("error: optional has no contained value");
+    }
+};
+
+/* A minimal replica of optional found in C++17 */
+template<typename T>
+class optional
+{
+ public:
+    using value_type = T;
+
+    optional() noexcept {}
+    optional(const value_type& value) {
+        _mHasValue = true;
+        _assign(value);
+    }
+    optional(nullopt_t) noexcept {}
+
+    optional<T>& operator=(nullopt_t) noexcept {
+        reset();
+        return(*this);
+    }
+    optional<T>& operator=(const value_type& value) {
+        _mHasValue = true;
+        _assign(value);
+        return(*this);
+    }
+
+    const value_type* operator->() const {
+        return(reinterpret_cast<const T*>(_mValue));
+    }
+    value_type* operator->() {
+        return(reinterpret_cast<T*>(_mValue));
+    }
+    const value_type& operator*() const {
+        return(*reinterpret_cast<const T*>(_mValue));
+    }
+    value_type& operator*() {
+        return(*reinterpret_cast<T*>(_mValue));
+    }
+
+
+    explicit operator bool() const noexcept {return(_mHasValue);}
+    bool has_value() const noexcept {return(_mHasValue);}
+
+    T& value() {
+        if(!_mHasValue)
+            throw bad_optional_access();
+        T* interpreted_value = reinterpret_cast<T*>(_mValue);
+        return(*interpreted_value);
+    }
+    const T& value() const {
+        if(!_mHasValue)
+            throw bad_optional_access();
+        const T* interpreted_value = reinterpret_cast<const T*>(_mValue);
+        return(*interpreted_value);
+    }
+
+    void reset() noexcept {
+        if(_mHasValue){
+            value().~T();
+        }
+        _mHasValue = false;
+    }
+
+    friend bool operator==(const optional<T>& lhs, const optional<T>& rhs){
+        bool sameHaveValue = lhs.has_value() == rhs.has_value();
+        return(sameHaveValue && (!lhs.has_value() || *lhs == *rhs) );
+    }
+    friend bool operator==(const optional<T>& lhs, const value_type& rhs) {return(lhs.has_value() && *lhs == rhs);}
+    friend bool operator==(const value_type& lhs, const optional<T>& rhs) {return(rhs.has_value() && lhs == *rhs);}
+    friend bool operator==(const optional<T>& lhs, nullopt_t) {return(!lhs.has_value());}
+    friend bool operator==(nullopt_t, const optional<T>& rhs) {return(!rhs.has_value());}
+
+    friend bool operator!=(const optional<T>& lhs, const optional<T>& rhs) {return(!operator==(lhs, rhs));}
+    friend bool operator!=(const optional<T>& lhs, const value_type& rhs) {return(!operator==(lhs, rhs));}
+    friend bool operator!=(const value_type& lhs, const optional<T>& rhs) {return(!operator==(lhs, rhs));}
+    friend bool operator!=(const optional<T>& lhs, nullopt_t) {return(!operator==(lhs, nullopt));}
+    friend bool operator!=(nullopt_t, const optional<T>& rhs) {return(!operator==(nullopt, rhs));}
+    
+ protected:
+    void _assign(const value_type& value) {
+        *reinterpret_cast<T*>(_mValue) = value; 
+    }
+    
+ private:
+    bool _mHasValue = false; 
+    uint8_t _mValue[sizeof(value_type)];
+};
+
+} // end namespace std
 
 #endif
+
+#endif 
