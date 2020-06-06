ddb42bbc2340760a70fe7cda4a064f8d4c21a59c
Kolton Yager <kjyager@calpoly.edu>
2020-06-06 04:38:29 -0700

    Updated doc comment for spec constant concat

--- vkutils.h
+++ vkutils.h
@@ -100,12 +100,12 @@ inline void find_feature_matches(
 
 uint32_t total_descriptor_count(const std::vector<VkDescriptorPoolSize>& aPoolSizes);
 
-/// Merges specialization info specified in `a` with specialization info in `b`.
+/// Concatenates specialization info specified in `a` with specialization info in `b`, storing the result in `out`
 ///
 /// Ownership of memory created by this function is moved to the caller via a vector of uchars and 
-/// a vector of map entries. The out parameter is filled with the appropraite values, but the return
+/// a vector of map entries. The out parameter is filled with the appropriate values, but the return
 /// values must remain in-scope or bad-reads may occur when the pipeline is built.
-/// \returns A pair of vectorors holding the data for the output specialization info. MUST stay in scope
+/// \returns A pair of vectors holding the data for the output specialization info. MUST stay in scope
 /// until the pipeline is built. 
 std::pair<std::vector<VkSpecializationMapEntry>, std::vector<uint8_t>> concat_specialization_info(
     const VkSpecializationInfo& a,
